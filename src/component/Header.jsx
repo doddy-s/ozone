e787022b2337dd6d4669e58f656ab096c37c64ef
OxaDefrizal <oxadefrizal@students.amikom.ac.id>
2023-05-28 21:07:54 +0700

    Header finish but no responsive. next side bar

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -3,7 +3,19 @@ import "./Header.css";
 const Header = () => {
     return(
         <div className="container-header">
-            <h1>ini header</h1>
+            <div className="container-logo">
+                <a href="" className="link-logo">
+                    <img className="logo" src="/src/assets/ozone_logo.png" alt="ini logo" />
+                </a>
+            </div>
+            <div className="container-searchbar">
+                <form action="" className="search-form">
+                    <input type="text" placeholder="Search" />
+                        <button type="submit">
+                            <img src="/src/assets/search.svg" alt="Search Icon" />
+                        </button>
+                </form>
+            </div>
         </div>
 
     );
