b74c2d393704ee9dd8c992f446c4766d40dda516
doddy-s <doddy.s@students.amikom.ac.id>
2023-06-12 21:33:56 +0700

    database asulah mending front-end hadeh

--- server/sequelize/migrations/20230612024023-create-user.js
+++ server/sequelize/migrations/20230612024023-create-user.js
@@ -12,31 +12,51 @@ module.exports = {
         field: 'user_id'
       },
       name: {
-        type: DataTypes.STRING(32),
+        type: Sequelize.STRING(32),
         allowNull: true,
-        unique: false
+        unique: false,
+        field: 'name'
       },
       gender: {
-        type: DataTypes.BOOLEAN,
+        type: Sequelize.BOOLEAN,
         allowNull: true,
-        unique: false
+        unique: false,
+        field: 'gender'
       },
       bio: {
-        type: DataTypes.STRING,
+        type: Sequelize.TEXT,
         allowNull: true,
-        unique: false
+        unique: false,
+        field: 'bio'
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: 'created_at'
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+        field: 'updated_at'
       },
       accountId: {
         type: Sequelize.UUID,
         allowNull: false,
         field: 'account_id'
-      },
-      socialId: {
-        type: Sequelize.UUID,
-        allowNull: false,
-        field: 'account_id'
       }
-    });
+    })
+    await queryInterface.addConstraint('users', {
+      fields: ['account_id'],
+      type: 'foreign key',
+      references: {
+        table: 'accounts',
+        field: 'account_id'
+      },
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    })
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Users');
