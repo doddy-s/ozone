8d19ecbe1abfaa63e0cf267e784ce2e893bf58a6
Irfan476 <irfanandryanto47@students.amikom.ac.id>
2023-06-20 12:52:34 +0700

    post controller

--- server/src/controllers/post.controller.js
+++ server/src/controllers/post.controller.js
@@ -1,3 +1,110 @@
+const express = require('express');
+const { Sequelize } = require('sequelize');
+const { Post } = require('../../sequelize/models');
+
+const router = express.Router();
+const sequelize = new Sequelize(config.development);
+
+/**
+ * Get posts by popularity
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+const getPostByPopularity = async (req, res) => {
+  try {
+    const posts = await Post.findAll({
+      order: [['up', 'DESC']],
+    });
+
+    if (posts.length === 0) {
+      throw new Error('No posts found');
+    }
+
+    const response = {
+      code: 200,
+      status: 'Ok',
+      data: posts
+    };
+    return res.status(200).json(response);
+  } catch (error) {
+    const response = {
+      code: 404,
+      status: 'Not Found',
+      message: error.message || 'No posts found'
+    };
+
+    console.log(response);
+
+    return res.status(response.code).json(response);
+  }
+};
+
+/**
+ * Get posts by tag
+ * @param {*} req
+ * @param {*} res
+ * @returns
+ */
+const getPostsByTag = async (req, res) => {
+  try {
+    const { tag } = req.params;
+
+    const posts = await Post.findAll({
+      where: {
+        tag: tag
+      }
+    });
+
+    const response = {
+      code: 200,
+      status: 'Ok',
+      data: posts
+    };
+    return res.status(200).json(response);
+  } catch (error) {
+    const response = {
+      code: 404,
+      status: 'Not Found',
+      message: error.message || 'No posts found for the specified tag'
+    };
+
+    console.log(response);
+
+    return res.status(response.code).json(response);
+  }
+};
+
+const createPost = async (req, res) => {
+  try {
+    const { tag, up, down, content, media, userId, communityId } = req.body;
+
+    const post = await Post.create({
+      tag,
+      up,
+      down,
+      content,
+      media,
+      userId,
+      communityId
+    });
+
+    res.status(201).json({ post });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const getPostById = async (req, res) => {
+  try {
+    const { postId } = req.params;
+
+    const post = await Post.findByPk(postId);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found' });
+    }
 
     res.status(200).json({ post });
   } catch (error) {
@@ -5,3 +112,11 @@
     res.status(500).json({ message: 'Internal server error' });
   }
 };
+
+router.get('/posts/popular', getPostByPopularity);
+router.get('/posts/tag/:tag', getPostsByTag);
+router.get('/posts/:postId', getPostById);
+router.post('/posts', createPost);
+
+module.exports = router;
+
--- server/src/routes/post.route.js
+++ server/src/routes/post.route.js
@@ -1,35 +1,21 @@
 const express = require('express');
-const routes = express.Router();
-const postController = require('../controllers/post.controller');
+const Sequelize = require('sequelize');
+const post = require('../../sequelize/models/post');
+const sequelize = new Sequelize('ozone_dev', 'root', 'FexBtfbjCgtXxpjvdcTW', {
+  host: 'containers-us-west-178.railway.app',
+  port: 7427,
+  dialect: 'mysql'
+});
 
-routes.post('/posts', postController.createPost);
+const router = express.Router();
 
-routes.get('/posts/:postId', postController.getPostById);
-
-module.exports = routes;
-
-
-
-// const express = require('express');
-// const Sequelize = require('sequelize');
-// const post = require('../../sequelize/models/post');
-// const sequelize = new Sequelize('ozone_dev', 'root', 'FexBtfbjCgtXxpjvdcTW', {
-//   host: 'containers-us-west-178.railway.app',
-//   port: 7427,
-//   dialect: 'mysql'
-// });
-
-// const router = express.Router();
-
-// router.get('/getposts', async (req, res) => {
-//   try {
-//     const posts = await post.findAll();
-//     res.json(posts);
-//   } catch (error) {
-//     res.status(500).json({ error: 'Failed to retrieve posts' });
-//   }
-// });
+router.get('/getposts', async (req, res) => {
+  try {
+    const posts = await post.findAll();
+    res.json(posts);
+  } catch (error) {
+   res.status(500).json({ error: 'Failed to retrieve posts' });
+  }
+});
 
 module.exports = router;
-
-// module.exports = router;
