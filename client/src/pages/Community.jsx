85befe09ead64532a796616b2428354662677428
OxaDefrizal <oxadefrizal@students.amikom.ac.id>
2023-06-28 11:16:39 +0700

    add Modul to Create Community ini Community page

--- client/src/pages/Community.jsx
+++ client/src/pages/Community.jsx
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 import style from "../assets/css/Community.module.css";
 
 function Community() {
+  // JS for change join community button
   const [isJoined, setIsJoined] = useState(false);
 
   const handleClick = () => {
     setIsJoined(!isJoined);
   };
+
+  //JS for Dialog to Create Community
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if (modal) {
+    document.body.classList.add("active-modal");
+  } else {
+    document.body.classList.remove("active-modal");
+  }
 
   return (
     <>
@@ -307,6 +321,8 @@ function Community() {
               </div>
             </button>
           </div>
+
+          {/* Create Community Button */}
           <div className={style.containerCreateCommunity}>
             <div className={style.createCommunityContent}>
               <img src="/src/assets/images/image-create-community.png" alt="" />
@@ -316,11 +332,84 @@ function Community() {
               <p className={style.descCreateCommunity}>
                 make your community wider invite everyone to join
               </p>
-              <button className={style.createCommunityBtn}>
+              <button
+                onClick={toggleModal}
+                className={style.createCommunityBtn}
+              >
                 Create Community
               </button>
             </div>
           </div>
+
+          {/* Modal Create Community */}
+          {modal && (
+            <div className={style.modal}>
+              <div onClick={toggleModal} className={style.overlay}></div>
+              <div className={style.containerModal}>
+                <div className={style.contentModal}>
+                  <div className={style.headerModel}>
+                    <p className={style.titleCreateCommunityModal}>
+                      Create Community
+                    </p>
+                    <button className={style.closeModal} onClick={toggleModal}>
+                      <img src="/src/assets/images/close.svg" alt="close" />
+                    </button>
+                  </div>
+
+                  <div className={style.addBannerCommunity}>
+                    <img
+                      className={style.bannerCommunity}
+                      src="/src/assets/images/add-image-community.svg"
+                      alt=""
+                    />
+                  </div>
+                  <div className={style.addProfilePictCommunity}>
+                    <img
+                      className={style.bannerCommunity}
+                      src="/src/assets/images/add-image-community.svg"
+                      alt=""
+                    />
+                  </div>
+                  <form action="" className={style.addDescCommunity}>
+                    <input
+                      id="description"
+                      type="text"
+                      placeholder="Describe your community"
+                    />
+                  </form>
+                  <div className={style.communityType}>
+                    Community Type
+                    <form action="" className={style.communityTypeRadio}>
+                      <input
+                        type="radio"
+                        name="type"
+                        id="public"
+                        value="public"
+                      />
+                      <img
+                        className={style.typeRadioIcon}
+                        src="/src/assets/images/user.svg"
+                        alt="icon"
+                      />
+                      <label htmlFor="public">public</label>
+                      <input
+                        type="radio"
+                        name="type"
+                        id="private"
+                        value="private"
+                      />
+                      <img
+                        className={style.typeRadioIcon}
+                        src="/src/assets/images/private.svg"
+                        alt="icon"
+                      />
+                      <label htmlFor="private">private</label>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </>
