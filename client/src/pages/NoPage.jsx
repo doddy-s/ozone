85588dc3057aa5e0c40f8667c258827fbe67d0c1
Dicky Akbar Baihaqki <dickyakbarb@students.amikom.ac.id>
2023-06-30 21:32:31 +0700

    lottie eror page

--- client/src/pages/NoPage.jsx
+++ client/src/pages/NoPage.jsx
@@ -1,13 +1,21 @@
+import React from "react";
+import Lottie from "lottie-react";
+import boteror404 from "../assets/images/98488-bot-error-404.json";
 import style from '../assets/css/Nopage.module.css'
 
 function Nopage() {
 
     return (
       <>
-        <div className={style['container-nopage']}>
+        <div>
           <h1>Page not found</h1>
-          
         </div>
+        <div className={style['container-lottie']}>
+          <Lottie animationData={boteror404} loop={true} />
+        </div>
+        <h2>The page you are looking for doesn’t exist or an other error occured</h2>
+        <h3>go back</h3>
+        
       </>
     );
   }
