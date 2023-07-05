74b33ca207683d872058bac7859b286d13b64c29
doddy-s <doddy.s@protonmail.com>
2023-07-05 19:00:35 +0700

    udah bisa posting

--- client/src/api/community.js
+++ client/src/api/community.js
@@ -1,6 +1,26 @@
-export const getCommunities = async (req, res) => {
+export const getCommunities = async () => {
   try {
-    const response = await fetch("http://localhost:3000/communities/by-member-count", {
+    const response = await fetch(
+      "http://localhost:3000/communities/by-member-count",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      }
+    );
+    const { data } = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const getJoinedCommunities = async () => {
+  try {
+    const response = await fetch("http://localhost:3000/communities/joined", {
       method: "GET",
       headers: {
         "Content-Type": "application/json",
