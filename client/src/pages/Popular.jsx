1bcfba923a178887bec750b261f73b4a1ea9e092
OxaDefrizal <oxadefrizal@students.amikom.ac.id>
2023-07-07 15:18:30 +0700

    add popular page

--- client/src/pages/Popular.jsx
+++ client/src/pages/Popular.jsx
@@ -1,11 +1,32 @@
-import style from '../assets/css/Popular.module.css'
-import Feed from "../components/Feed";
+import style from "../assets/css/Popular.module.css";
+import Post from "../components/Post";
+import { getPosts } from "../api/post";
+import { useEffect, useState } from "react";
 
-function Popular() {
+function Popular(user) {
+  const [post, setPost] = useState([]);
+
+  async function fetchData() {
+    const data = await getPosts();
+    setPost(data);
+  }
+
+  useEffect(() => {
+    try {
+      fetchData();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
   return (
     <>
       <div className={style.containerPopular}>
-        <Feed/>
+        <div className={style.posts}>
+          {post?.map((i) => (
+            <Post key={i.postId} post={i} />
+          ))}
+        </div>
       </div>
     </>
   );
