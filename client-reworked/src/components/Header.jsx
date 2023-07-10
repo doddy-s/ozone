5b3d3ee56fafaf3c4683e9a320cbcbb2f93fc8a1
doddy-s <doddy.s@protonmail.com>
2023-07-10 18:34:08 +0700

    adds setting

--- client-reworked/src/components/Header.jsx
+++ client-reworked/src/components/Header.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { SearchBox } from "./SearchBox";
+import { Setting } from "./Setting";
 
 export const Header = () => {
   return (
     <>
-      <div className="h-full w-full flex items-center justify-start p-2">
+      <div className="h-full w-full flex items-center justify-start p-2 bg-white dark:bg-black">
         <div className="h-full w-60 flex items-center justify-center">
           <img
             src="https://cdn.discordapp.com/attachments/1126661998063652924/1126670001848455220/ozone-logo.svg"
@@ -12,8 +13,9 @@ export const Header = () => {
             className="p-0"
           />
         </div>
-        <div className="w-full flex items-center justify-start pl-2">
+        <div className="w-full flex items-center justify-between px-2">
           <SearchBox />
+          <Setting />
         </div>
       </div>
     </>
