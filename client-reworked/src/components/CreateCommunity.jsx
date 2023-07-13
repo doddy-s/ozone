b077a8918697ec1ce52fa97ab68827abb5abfa43
doddy-s <doddy.s@protonmail.com>
2023-07-13 18:17:39 +0700

    create community

--- client-reworked/src/components/CreateCommunity.jsx
+++ client-reworked/src/components/CreateCommunity.jsx
@@ -3,14 +3,19 @@ import React from "react";
 export const CreateCommunity = () => {
   return (
     <>
-      <div className="h-48 w-full flex flex-col items-center justify-between">
+      <div className="h-auto w-full flex flex-col items-center justify-between rounded-xl overflow-hidden gap-2 bg-white pb-2">
         <img
-          src="https://cdn.discordapp.com/attachments/1069940642521825320/1069940739263443035/pepe01.png"
+          src="https://cdn.discordapp.com/attachments/1069940642521825320/1070109666299887636/kung-fu-panda-master-oogway.gif"
           alt="banncc"
           className="w-full aspect-video"
         />
-        <h1>Connect people with community</h1>
-        <p>make your community wider invite everyone to join</p>
+        <h1 className="text-sm text-center">Connect people with community</h1>
+        <p className="text-xs text-center">
+          make your community wider invite everyone to join
+        </p>
+        <button className="px-2 rounded-full text-xl border-2 border-blue-400 text-blue-400">
+          Create Community
+        </button>
       </div>
     </>
   );
--- client-reworked/src/components/Header.jsx
+++ client-reworked/src/components/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <>
       <div className="h-full w-full flex items-center justify-start p-2 bg-white">
-        <div className="h-full w-60 flex items-center justify-center">
+        <div className="h-full w-20 md:w-60 flex items-center justify-center">
           <img
             src="https://cdn.discordapp.com/attachments/1126661998063652924/1126670001848455220/ozone-logo.svg"
             alt="ozone_logo"
@@ -18,7 +18,7 @@ export const Header = () => {
             className="p-0 h-8 aspect-square block md:hidden"
           />
         </div>
-        <div className="w-full flex items-center justify-between px-2">
+        <div className="w-full flex items-center justify-between px-2 gap-4">
           <SearchBox />
           <Setting />
         </div>
